9758439c86d4c12b8d9fdc47165e06552846e987
Elchar581 <maksimcuhrov@gmail.com>
2019-11-07 23:12:52 +0300

    Init Bush CC version 1.2

--- P1Hait.py
+++ P1Hait.py
@@ -5,36 +5,36 @@ from PyQt5.QtWidgets import QWidget, QApplication, QPushButton, QLabel, QLineEdi
 from PyQt5 import QtCore, QtGui
 
 
-class P1Main(QMainWindow):
+class P1Main(QMainWindow):  # Меню - окно с выбором
     def __init__(self):
         super(P1Main, self).__init__()
         uic.loadUi('P1Main.ui', self)
         self.initUI()
 
-    def initUI(self):
+    def initUI(self):  # Метод отвечающий за нажатие кнопок
         self.E1R.clicked.connect(self.run)
         self.E2R.clicked.connect(self.run2)
 
-    def run(self):
+    def run(self):  # Открывает окно с переводчиком
         self.B1 = P1B1()
         self.B1.show()
 
-    def run2(self):
+    def run2(self):  # Открывает окно с калькулятором
         self.B2 = P1B2()
         self.B2.show()
 
 
-class P1B1(QWidget):
+class P1B1(QWidget):  # Окно с переводчиком С.С.
     def __init__(self):
         super(P1B1, self).__init__()
         uic.loadUi('P1B1.ui', self)
         self.initUI()
 
-    def initUI(self):
+    def initUI(self):  # Метод отвечающий за нажатие кнопок
         self.pushButton.clicked.connect(self.shat)
         self.E1B.clicked.connect(self.run)
 
-    def run(self):
+    def run(self):  # Открывает окно меню
         self.B0 = P1Main()
         self.B0.show()
 
@@ -49,7 +49,7 @@ class P1B1(QWidget):
         c2 = self.C2B.currentText()
         a1 = str(self.X1B.text())
         count = 0
-        if count == 0:
+        if count == 0:  # проверка на корректность условия
             if c1 == 'Двоичная':
                 for i in str(a1):
                     if i not in t2:
@@ -67,7 +67,7 @@ class P1B1(QWidget):
                     if i not in t16:
                         count += 1
         if count != 0:
-            self.kin1.setText(str('Некорректное число'))
+            self.kin1.setText(str('Некорректное условие'))
         else:
             b1 = t0.index(c1)
             b2 = int(t0.index(c2))
@@ -76,7 +76,7 @@ class P1B1(QWidget):
             ux1 = 0
             a3 = 0
             a4 = []
-            if b1 == 16:
+            if b1 == 16:  # перевод в десятичную С.С.
                 for i in a2:
                     n = int(t16.index(str(i)))
                     a3 += n * (b1 ** ux1)
@@ -85,7 +85,7 @@ class P1B1(QWidget):
                 for i in a2:
                     a3 += int(i) * (b1 ** ux1)
                     ux1 += 1
-            if c2 == 'Двоичная':
+            if c2 == 'Двоичная':  # Перевод кода СС по условию
                 while a3 >= 2:
                     a4.append(str(a3 % 2))
                     a3 -= a3 % 2
@@ -120,19 +120,19 @@ class P1B1(QWidget):
                 self.kin1.setText(str(''.join(a4[::-1])))
 
 
-class P1B2(QWidget):
+class P1B2(QWidget):  # Открывает окно с калькулятором С.С.
     def __init__(self):
         super(P1B2, self).__init__()
         uic.loadUi('P1B2.ui', self)
         self.initUI()
+
+    def initUI(self):  # Метод отвечающий за нажатие кнопок
+        self.pushButton.clicked.connect(self.shat)
         self.E1B.clicked.connect(self.run)
 
-    def run(self):
+    def run(self):  # Открывает окно меню
         self.B0 = P1Main()
         self.B0.show()
-
-    def initUI(self):
-        self.pushButton.clicked.connect(self.shat)
 
     def shat(self):
         t0 = ['0', '1', 'Двоичная', '3', '4', '5', '6', '7', 'Восьмиричная', '9',
@@ -147,7 +147,7 @@ class P1B2(QWidget):
         a1 = str(self.X1B.text())
         az = str(self.X3B.text())
         count = 0
-        if count == 0:
+        if count == 0:  # проверка на корректность условия
             if c1 == 'Двоичная':
                 for i in str(a1):
                     if i not in t2:
@@ -181,8 +181,8 @@ class P1B2(QWidget):
                     if i not in t16:
                         count += 1
         if count != 0:
-            self.X2B.setText(str('Некорректное число'))
-        else:
+            self.X2B.setText(str('Некорректное условие'))
+        else:  # перевод в десятичную С.С.
             b1 = t0.index(c1)
             b2 = int(t0.index(c2))
             a1 = str(a1)
@@ -214,15 +214,15 @@ class P1B2(QWidget):
                     az3 += int(i) * (b3 ** ux2)
                     ux2 += 1
             Z1 = self.Z1B.currentText()
-            if Z1 == '+':
+            if Z1 == '+':  # Сумма двух кодов С.С.
                 a3 += az3
-            if Z1 == '-':
+            if Z1 == '-':  # Разность двух кодов С.С.
                 a3 -= az3
-            if Z1 == 'x':
+            if Z1 == 'x':  # Произведение двух кодов С.С.
                 a3 *= az3
-            if Z1 == ':':
+            if Z1 == ':':  # Частное двух кодов С.С.
                 a3 /= az3
-            if c2 == 'Двоичная':
+            if c2 == 'Двоичная':  # Перевод кода СС по условию
                 while a3 >= 2:
                     a4.append(str(a3 % 2))
                     a3 -= a3 % 2
@@ -261,4 +261,3 @@ app = QApplication(sys.argv)
 ex = P1Main()
 ex.show()
 sys.exit(app.exec_())
-
